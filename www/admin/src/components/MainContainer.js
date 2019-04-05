ce79eaf7a8560c4d9b038f9db9d2dfaa0a834346
Dan Maldonado <dmaldonado@epc-instore.com>
2019-04-04 23:14:56 -0400

    Removing unused imports

--- www/admin/src/components/MainContainer.js
+++ www/admin/src/components/MainContainer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import BlankPanel from './panels/Blank';
 import Login from './LoginContainer';
