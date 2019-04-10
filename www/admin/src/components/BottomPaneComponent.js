f7bdf0376b011aeee9e3392a68d7ae84c5db258d
Dan Maldonado <dmaldonado@epc-instore.com>
2019-04-09 22:12:07 -0400

    Basic intro

--- www/admin/src/components/BottomPaneComponent.js
+++ www/admin/src/components/BottomPaneComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Intro from './Intro';
 import Users from './users/List';
 
--- www/admin/src/components/Intro.js
+++ www/admin/src/components/Intro.js
@@ -1,12 +1,30 @@
 import React from 'react';
 //import Location from './Location';
+import {Card} from 'primereact/card';
 
 export default () => {
    return (
-      <div>
-              
-Intro
-
-      </div>
+      <Card>
+      <h2>WebGUI is a mature, feature rich Content Management System.</h2>
+      <h3>It's written in Perl and licensed under the GNU GPL. Some features include:</h3>  
+         <ul>
+         <li>Hierarchical permissions</li>
+         <li>Groups of groups of groups (etc)</li>
+         <li>Asset versioning</li>
+         <li>A workflow builder that can do things like require two Content Managers to give approval before user submitted content goes online</li>
+         <li>Selectable workflows for new/edited assets</li>
+         <li>Assets create other assets; almost everything is an asset</li>
+         <li>Easily, cleanly extensible OO architecture</li>
+         <li>Discussion boards, shops, image galleries, ticket trackers, and various other types of interactive content</li>
+         <li>Scalable architecture suitable for busy sites</li>
+         </ul>
+     
+      You can find more information about the CMS at:  <a href="https://github.com/AlliumCepa/webgui">WebGUI</a>
+      <br />
+      <br />
+      <h4>This interface was written using the excellent JavaScript library: <a href="https://reactjs.org">React</a></h4>
+      <h4>The fantastic component library <a href="https://www.primefaces.org/primereact">PrimeReact</a> can be found <a href="https://www.primefaces.org/primereact">here</a></h4>
+     
+      </Card>
    );   
 }
