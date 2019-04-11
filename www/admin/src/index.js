e8c6473c2ec46ed6fd7e32dcfdc54cfba999b06c
Dan Maldonado <dmaldonado@epc-instore.com>
2019-04-10 23:55:08 -0400

    Preserve state across browser refresh.

--- www/admin/src/index.js
+++ www/admin/src/index.js
@@ -16,7 +16,27 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)) );
+const loadFromLocalStorage = () => {
+   try{
+      const serializedState = localStorage.getItem('kestrel-state');
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+   }catch(e){
+      console.log(e);
+      return undefined;
+   }
+   
+};
+const persistedState = loadFromLocalStorage();
+const store = createStore(reducers, persistedState, composeEnhancers(applyMiddleware(thunk)) );
+
+const saveToLocalStorage = state => {
+   try {
+      localStorage.setItem('kestrel-state', JSON.stringify(state));
+   } catch(e){
+      console.log(e);
+   }
+};
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 render(
    <Provider store={store}>
