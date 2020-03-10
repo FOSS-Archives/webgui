9ad069efa2424ae598b4a683f983d51f5f2ae05d
Dan Maldonado <dmaldonado@epc-instore.com>
2020-03-10 19:29:02 -0400

    Fixes the '==' vs '===' warning.

--- www/developer/admin/src/components/users/UI.js
+++ www/developer/admin/src/components/users/UI.js
@@ -9,7 +9,7 @@ const UI = user => {
             <div className="p-col-3 text-right font-weight-bold">Language:</div>
             <div className="p-col-9">{user.language}</div>
             <div className="p-col-3 text-right font-weight-bold">Are you available to be added as a Friend?:</div>
-            <div className="p-col-9">{user.friendly == true ? <span>Yes</span> : <span>No</span>}</div>
+            <div className="p-col-9">{user.friendly === true ? <span>Yes</span> : <span>No</span>}</div>
             <div className="p-col-3 text-right font-weight-bold">Date Format:</div>
             <div className="p-col-9">{user.dateformat}</div>
             <div className="p-col-3 text-right font-weight-bold">Time Format:</div>
@@ -21,11 +21,11 @@ const UI = user => {
             <div className="p-col-3 text-right font-weight-bold">Private Message Options:</div>
             <div className="p-col-9">{user.messageoptions}</div>
             <div className="p-col-3 text-right font-weight-bold">Show when online?:</div>
-            <div className="p-col-9">{user.showwhenonline == true ? <span>Yes</span> : <span>No</span>}</div>
+            <div className="p-col-9">{user.showwhenonline === true ? <span>Yes</span> : <span>No</span>}</div>
             <div className="p-col-3 text-right font-weight-bold">Receive inbox notifications as email?:</div>
-            <div className="p-col-9">{user.notifyemail == true ? <span>Yes</span> : <span>No</span>}</div>
+            <div className="p-col-9">{user.notifyemail === true ? <span>Yes</span> : <span>No</span>}</div>
             <div className="p-col-3 text-right font-weight-bold">Receive inbox notifications as SMS?:</div>
-            <div className="p-col-9">{user.notifysms == true ? <span>Yes</span> : <span>No</span>}</div> 
+            <div className="p-col-9">{user.notifysms === true ? <span>Yes</span> : <span>No</span>}</div> 
          </div>
       </div>
    );
