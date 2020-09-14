2ee67f47ed4670bb3159983e35e8ffd38ca64396
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-13 23:52:40 -0400

    Group properly deleted

--- www/developer/admin/src/actions/groups.js
+++ www/developer/admin/src/actions/groups.js
@@ -1,10 +1,28 @@
 import * as constants from './constants';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const groups = () => async dispatch => {
+const groups = () => async dispatch => {
    const response = await jsonPlaceholder.get(process.env.REACT_APP_groups);
    dispatch({
       type: constants.GROUPS,
       payload: response.data
    });
 };
+
+const deleteGroup = id => async dispatch => {
+   await jsonPlaceholder.delete(process.env.REACT_APP_groups + '/' + id)
+      .then( response => {
+         dispatch({
+            type: constants.MESSAGE_OK,
+            payload: { detail: "Group deleted", type: "OK" }
+         });
+      })
+      .catch( error => {
+         dispatch({
+            type: constants.ERROR,
+            payload: { detail: error.response.statusText, sumary: error.response.status }
+         });
+      });
+};
+
+export { deleteGroup, groups };
