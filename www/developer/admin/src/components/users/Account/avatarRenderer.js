b75436df98e4c3eb434902462fe1c46d86c566fd
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-20 12:48:28 -0400

    Refine account area to include dynamic content such a photo and avatar.

--- www/developer/admin/src/components/users/Account/avatarRenderer.js
+++ www/developer/admin/src/components/users/Account/avatarRenderer.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Inplace, InplaceDisplay, InplaceContent } from 'primereact/inplace';
-import { InputText } from 'primereact/inputtext';
+import AvatarRenderer from '../../media/avatarRenderer';
 
 export default ({user, updateField}) => {
    return (
       <div className="p-grid allium-editable-field">
-         <div className="p-col-2 p-md-2 p-lg-2 font-weight-bold text-nowrap">Avatar:</div>         
-         <Inplace closable className="p-col-10 p-md-10 p-lg-4">
-            <InplaceDisplay>
-               {user.avatar || 'Click to Edit'}
-            </InplaceDisplay>
-            <InplaceContent>
-               <InputText value={user.avatar} onChange={(e) => updateField('avatar', e.target.value)} autoFocus />
-            </InplaceContent>
-         </Inplace>    
+         <div className="p-col-2 p-md-2 p-lg-2 font-weight-bold text-nowrap">Avatar:</div>
+         <AvatarRenderer src={user.avatar} altText="Avatar" height="100" width="100" className="p-col-10 p-md-10 p-lg-4" />
       </div>
    );
 };
--- www/developer/admin/src/components/users/Account/photoRenderer.js
+++ www/developer/admin/src/components/users/Account/photoRenderer.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Inplace, InplaceDisplay, InplaceContent } from 'primereact/inplace';
-import { InputText } from 'primereact/inputtext';
+import PhotoRenderer from '../../media/photoRenderer';
 
 export default ({user, updateField}) => {
    return (
       <div className="p-grid allium-editable-field">
          <div className="p-col-2 p-md-2 p-lg-2 font-weight-bold text-nowrap">Photo:</div>         
-         <Inplace closable className="p-col-10 p-md-10 p-lg-4">
-            <InplaceDisplay>
-               {user.photo || 'Click to Edit'}
-            </InplaceDisplay>
-            <InplaceContent>
-               <InputText value={user.photo} onChange={(e) => updateField('photo', e.target.value)} autoFocus />
-            </InplaceContent>
-         </Inplace>    
+         <PhotoRenderer src={user.avatar} altText="Avatar" height="100" width="100" className="p-col-10 p-md-10 p-lg-4" />
       </div>
    );
 };
