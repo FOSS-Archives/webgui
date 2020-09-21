1899e43a869d2969ec318f0effeaeb860dedc037
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-21 12:45:37 -0400

    Field reorg have a better visual flow

--- www/developer/admin/src/components/users/Account/accountRenderer.js
+++ www/developer/admin/src/components/users/Account/accountRenderer.js
@@ -10,12 +10,12 @@ export default ({user, updateField}) => {
    return (
       <div className="p-grid nested-grid">
          <div className="p-col-8">
-            <EditableField object={user} fieldName="username" fieldLabel="Username:" updateFieldState={updateField} />     
-            <EditableField object={user} fieldName="alias" fieldLabel="Alias:" updateFieldState={updateField} />     
+            <EditableField object={user} fieldName="username" fieldLabel="Username:" updateFieldState={updateField} />
+            <EditableField object={user} fieldName="alias" fieldLabel="Alias:" updateFieldState={updateField} />
+            <EditableField object={user} fieldName="firstname" fieldLabel="First Name:" updateFieldState={updateField} />              
+            <EditableField object={user} fieldName="lastname" fieldLabel="Last Name:" updateFieldState={updateField} />            
             <DateCreated {...user} />
             <LastUpated {...user} />         
-            <EditableField object={user} fieldName="firstname" fieldLabel="First Name:" updateFieldState={updateField} />              
-            <EditableField object={user} fieldName="lastname" fieldLabel="Last Name:" updateFieldState={updateField} />
             <EditableTextarea object={user} fieldName="signature" fieldLabel="Signature:" updateFieldState={updateField} />
          </div>
          <div className="p-col-4">
