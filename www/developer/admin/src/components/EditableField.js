a2a6be65beb234b58fb64e1d932fa2534710ac09
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-22 22:27:43 -0400

    Better yet, lets make sure many editable components are available

--- www/developer/admin/src/components/EditableField.js
+++ www/developer/admin/src/components/EditableField.js
@@ -1,16 +1,28 @@
 import React, {useState} from 'react';
-import { Inplace, InplaceDisplay, InplaceContent } from 'primereact/inplace';
-import { InputText } from 'primereact/inputtext';
+import {Dropdown} from 'primereact/dropdown';
+import {Inplace, InplaceDisplay, InplaceContent} from 'primereact/inplace';
+import {InputText} from 'primereact/inputtext';
 
 import './EditableField.css';
-export default ({object, fieldName, fieldLabel, updateFieldState, children}) => {
+export default ({object, fieldName, fieldLabel, updateFieldState, fieldType, fieldTypeData}) => {
    let [objectField, setObjectField] = useState(object[fieldName]);
 
    let saveFieldValue = fieldValue => {
       setObjectField(fieldValue);
       updateFieldState(fieldName, fieldValue);
-   };      
-    
+   };
+
+   let fieldComponent = null;
+   switch (fieldType){
+      case 'Dropdown':
+         const placeHolder = `Select ${fieldName}`;
+         fieldComponent = <Dropdown value={objectField} options={fieldTypeData} onChange={e => saveFieldValue(e.value)} placeholder={placeHolder}/>;
+      break;
+      
+      default: 
+         fieldComponent = <InputText value={objectField} onChange={e => saveFieldValue(e.target.value)} autoFocus />;
+   }
+
    return (
       <div className="p-grid allium-editable-field">
          <div className="p-col-2 p-md-2 p-lg-2 font-weight-bold text-nowrap">{fieldLabel}</div>
@@ -19,7 +31,7 @@ export default ({object, fieldName, fieldLabel, updateFieldState, children}) => 
                {object[fieldName] || 'Click to Edit'}
             </InplaceDisplay>
             <InplaceContent>
-               {children != null ? children : <InputText value={objectField} onChange={(e) => saveFieldValue(e.target.value)} autoFocus />}
+               {fieldComponent}
             </InplaceContent>
          </Inplace>
       </div>
