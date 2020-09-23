f5af0ec40b883fc704a54aa9521d1ad514cea34e
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-23 19:04:10 -0400

    no login necessary in this component

--- www/developer/admin/src/components/users/Profile/contactRenderer.js
+++ www/developer/admin/src/components/users/Profile/contactRenderer.js
@@ -3,9 +3,10 @@ import EditableField from '../../EditableField';
 
 import './Contact.css';
 export default ({user, updateField}) => {
+
    return (
       <div>
-         <EditableField object={user} fieldName="email" fieldLabel="Email:" updateFieldState={updateField} />  
+         <EditableField object={user} fieldName="email" fieldLabel="Email:" updateFieldState={updateField} />
          <EditableField object={user} fieldName="cell" fieldLabel="Cell:" updateFieldState={updateField} />  
          <EditableField object={user} fieldName="homephone" fieldLabel="Home Phone:" updateFieldState={updateField} />  
          <EditableField object={user} fieldName="workphone" fieldLabel="Work Phone:" updateFieldState={updateField} />           
