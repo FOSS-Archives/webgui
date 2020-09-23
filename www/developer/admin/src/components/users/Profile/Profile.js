f51d387cef5b0f6814b7b1edb99de8d199de8eb7
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-23 19:53:25 -0400

    Place the validation and evaluation of the fields in the Profile component.

---
+++ www/developer/admin/src/components/users/Profile/Profile.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import isEqual from 'react-fast-compare';
+import {validate} from 'validate.js';
+import {Button} from 'primereact/button';
+import ProfileRenderer from './profileRenderer';
+/*
+ * We are going to need some sort of validation and requirements of fields 
+ */
+import './Profile.css';
+export default ({user, saveUser}) => {
+   let [stateUser, updateUser] = useState(user);
+   let [dirty, setDirty] = useState(false);
+      
+   let updateField = (fieldName, fieldValue) => {
+      let modifiedUser = { ...stateUser, ...{[fieldName]: fieldValue} };
+      let modified = !isEqual(user, modifiedUser);
+      setDirty(modified);
+      updateUser(modifiedUser);
+   };
+   
+   let updateAppSessionUser = () => {
+      setDirty(false);      
+      saveUser(stateUser);
+   };
+            
+   let emailValidator = email => {
+      let constraints = { from: { email: true  } };
+      let valid = validate({from: email}, constraints);
+      if ( typeof valid !== 'undefined' ){
+         return { error: true, data: email, message: valid.from };          
+      }else{
+         return { error: false, data: email, message: null };
+      }
+      
+   }; 
+         
+   return (
+      <div>
+         <ProfileRenderer user={stateUser} updateField={updateField} />
+         {dirty && <Button id="allium-save-profile" label="Save" onClick={e => updateAppSessionUser()} />}
+      </div>
+   );
+
+};
