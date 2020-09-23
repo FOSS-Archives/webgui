62ea3cd484eefbfb20d03433f90a7cbc99e94d8b
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-23 19:04:36 -0400

    no login necessary in this component

--- www/developer/admin/src/components/users/Profile/profileRenderer.js
+++ www/developer/admin/src/components/users/Profile/profileRenderer.js
@@ -1,6 +1,4 @@
-import React, {useState} from 'react';
-import isEqual from 'react-fast-compare';
-import {Button} from 'primereact/button';
+import React from 'react';
 import {Fieldset} from "primereact/fieldset";
 import Contact from './contactRenderer';
 import Home from './homeRenderer';
@@ -8,40 +6,24 @@ import Personal from './personalRenderer';
 import Work from './workRenderer';
 
 import './Profile.css';
-export default ({user, index, saveUser}) => {
-   let [stateUser, updateUser] = useState(user);
-   let [dirty, setDirty] = useState(false);
-      
-   let updateField = (fieldName, fieldValue) => {
-      let modifiedUser = { ...stateUser, ...{[fieldName]: fieldValue} };
-      let modified = !isEqual(user, modifiedUser);
-      setDirty(modified);
-      updateUser(modifiedUser);
-   };
-   
-   let updateAppSessionUser = () => {
-      setDirty(false);      
-      saveUser(stateUser);
-   };
-      
+export default ({user, updateField}) => {
    return (
       <div>
          <Fieldset legend="Personal">
-            <Personal user={stateUser} updateField={updateField} />
+            <Personal user={user} updateField={updateField} />
          </Fieldset>
          
          <Fieldset legend="Contact">
-            <Contact user={stateUser} updateField={updateField} />
+            <Contact user={user} updateField={updateField} />
          </Fieldset>
 
          <Fieldset legend="Home">
-            <Home user={stateUser}  updateField={updateField} />
+            <Home user={user}  updateField={updateField} />
          </Fieldset>
 
          <Fieldset legend="Work">
-            <Work user={stateUser} updateField={updateField} />
+            <Work user={user} updateField={updateField} />
          </Fieldset>
-         {dirty && <Button id="allium-save-profile" label="Save" onClick={e => updateAppSessionUser()} />}
       </div>
    );
 };
