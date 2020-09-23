ee5ed375a07afa46089a0ad6ecd03bf05566f294
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-22 23:35:10 -0400

    Apparently the Calendar component returns an object.

--- www/developer/admin/src/components/EditableField.js
+++ www/developer/admin/src/components/EditableField.js
@@ -1,22 +1,35 @@
 import React, {useState} from 'react';
+import {Calendar} from 'primereact/calendar';
 import {Dropdown} from 'primereact/dropdown';
 import {Inplace, InplaceDisplay, InplaceContent} from 'primereact/inplace';
 import {InputText} from 'primereact/inputtext';
+import {InputTextarea} from 'primereact/inputtextarea';
 
 import './EditableField.css';
 export default ({object, fieldName, fieldLabel, updateFieldState, fieldType, fieldTypeData}) => {
    let [objectField, setObjectField] = useState(object[fieldName]);
 
    let saveFieldValue = fieldValue => {
+      if (fieldType === 'Calendar'){
+         fieldValue = fieldValue.toString();
+      }
       setObjectField(fieldValue);
       updateFieldState(fieldName, fieldValue);
    };
 
    let fieldComponent = null;
    switch (fieldType){
+      case 'Calendar':
+         fieldComponent = <Calendar value={objectField} onChange={e => saveFieldValue(e.value)} />;
+      break;
+      
       case 'Dropdown':
          const placeHolder = `Select ${fieldName}`;
          fieldComponent = <Dropdown value={objectField} options={fieldTypeData} onChange={e => saveFieldValue(e.value)} placeholder={placeHolder}/>;
+      break;
+      
+      case 'InputTextarea':
+         fieldComponent = <InputTextarea rows={5} cols={30} value={objectField} onChange={e => saveFieldValue(e.target.value)} autoFocus />;   
       break;
       
       default: 
@@ -26,7 +39,7 @@ export default ({object, fieldName, fieldLabel, updateFieldState, fieldType, fie
    return (
       <div className="p-grid allium-editable-field">
          <div className="p-col-2 p-md-2 p-lg-2 font-weight-bold text-nowrap">{fieldLabel}</div>
-         <Inplace closable className="p-col-10 p-md-10 p-lg-4">
+         <Inplace closable className="p-col-10 p-md-10 p-lg-8">
             <InplaceDisplay>
                {object[fieldName] || 'Click to Edit'}
             </InplaceDisplay>
