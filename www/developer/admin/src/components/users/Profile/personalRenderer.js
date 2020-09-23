c0095a3425cc575211bd86fd40fee4b753e95c78
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-22 23:39:18 -0400

    Good start to the personal option under profile.

--- www/developer/admin/src/components/users/Profile/personalRenderer.js
+++ www/developer/admin/src/components/users/Profile/personalRenderer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import EditableField from '../../EditableField';
+import GenderRenderer from './genderRenderer';
+import PronounRenderer from './pronounRenderer';
 
 import './Personal.css';
 export default ({user, updateField}) => {
    
    return (
       <div>
-         <EditableField object={user} fieldName="gender" fieldLabel="Gender:" updateFieldState={updateField} />  
-         <EditableField object={user} fieldName="pronoun" fieldLabel="Pronoun:" updateFieldState={updateField} />          
-         <EditableField object={user} fieldName="birthdate" fieldLabel="Birth Date:" updateFieldState={updateField} />  
+         <GenderRenderer user={user} updateFieldState={updateField} />  
+         <PronounRenderer user={user} updateFieldState={updateField} />          
+         <EditableField object={user} fieldName="birthdate" fieldLabel="Birth Date:" updateFieldState={updateField} fieldType="Calendar" />  
       </div>           
    );   
    
